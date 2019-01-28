3d1901be41233429650ffcf7e49ef2dc5d212474
Josephine Wright <jozright@gmail.com>
2019-01-28 13:19:49 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,165 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import statements from './statements'
+
+const unique = array => array.filter((item, i) => array.indexOf(item) === i);
+const levels = unique(statements.map(({ level }) => level));
+
+const avg = arr => arr.reduce((sum, v) => sum + v, 0) / arr.length;
+
+const calculateResultLevel = confidenceMap => {
+  console.log('confidenceMap',)
+  const sortedConfidenceMaps = Object.keys(confidenceMap)
+    .sort()
+    .reverse();
+  const finalLevel = sortedConfidenceMaps.find(level => {
+    const lvlConfMap = confidenceMap[level];
+    const percToLevel = percentage => (percentage / 100) * 4;
+    return (
+      avg(lvlConfMap) >= 3 ||
+      (avg(lvlConfMap) > percToLevel(60) && lvlConfMap.length >= 2)
+    );
+  });
+  return finalLevel || 0;
+};
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      initialQuestion: true,
+      questionsAsked: [],
+      previousLevels: [],
+      currentLevel: null,
+      confidenceMap: {}
+    };
+    this.getInitialQuestionChoices = this.getInitialQuestionChoices.bind(this);
+    this.getChoices = this.getChoices.bind(this);
+    this.handleInitialClick = this.handleInitialClick.bind(this);
+    this.handleNextLevel = this.handleNextLevel.bind(this);
+    this.getSliderChoices = this.getSliderChoices.bind(this);
+  }
+
+  defaultGetCanDo(level) {
+    const statementsByLevel = statements.reduce(
+      (obj, statement) => ({
+        ...obj,
+        [statement.level]: [...(obj[statement.level] || []), statement.question]
+      }),
+      {}
+    );
+    const previouslyAskedQuestionsForLevel = this.state.questionsAsked
+      .filter(q => q.level === level)
+      .map(({ question }) => question);
+    const questionsForLevel = statementsByLevel[levels[level]];
+    const askableQuestions =
+      previouslyAskedQuestionsForLevel.length < questionsForLevel.length
+        ? questionsForLevel.filter(
+            q => !previouslyAskedQuestionsForLevel.includes(q)
+          )
+        : questionsForLevel;
+    const getRandFromArray = array =>
+        array[Math.floor(Math.random() * array.length)];
+    return getRandFromArray(askableQuestions);
+  }
+
+  handleNextLevel(event) {
+    const { id } = event.target;
+    const idInt = parseInt(id);
+    const { currentLevel } = this.state;
+    let resultLevel;
+    const isSecondNeedPracticeInARow = () => {
+      if (idInt !== 2 || this.state.questionsAsked.length === 0) {
+        return false;
+      }
+      const { answer, level } = this.state.questionsAsked[
+        this.state.questionsAsked.length - 1
+      ];
+      return answer === 2 && currentLevel === level;
+    };
+    if (this.state.questionsAsked.length >= 5) {
+      return calculateResultLevel({
+        ...this.state.confidenceMap,
+        [currentLevel]: [...(this.state.confidenceMap[currentLevel] || []), idInt]
+      })
+    }
+    if (idInt > 2) {
+      resultLevel = Math.min(levels.length - 1, currentLevel + 1);
+    }
+    if (idInt < 2 || isSecondNeedPracticeInARow()) {
+      resultLevel = Math.max(0, currentLevel - 1);
+    }
+  
+    this.setState({
+      questionsAsked: [
+        ...this.state.questionsAsked,
+        {
+          question: this.defaultGetCanDo(this.state.currentLevel),
+          answer: idInt,
+          answerText: this.getSliderChoices()[idInt],
+          level: currentLevel,
+        },
+      ],
+      currentLevel: resultLevel,
+      confidenceMap: {
+        ...this.state.confidenceMap,
+        [currentLevel]: [
+          ...(this.state.confidenceMap[currentLevel] || []),
+          idInt
+        ]
+      }
+    });
+  }
+
+  handleInitialClick(event) {
+    const { id } = event.target;
+    const idInt = parseInt(id);
+    const cutPoint = Math.ceil(levels.length / 2);
+    const splitLevels = [levels.slice(0, cutPoint), levels.slice(cutPoint)];
+    const selectedSplitLevels = splitLevels[idInt];
+    const currentLevel = levels.indexOf(
+      selectedSplitLevels[Math.floor(selectedSplitLevels.length / 2)]
+    );
+    this.setState({
+      questionsAsked: [
+        ...this.state.questionsAsked,
+        {
+          question: "How much previous knowledge do you have of Spanish?",
+          answer: idInt,
+          answerText: ["a little", "a lot"][idInt]
+        },
+      ],
+      currentLevel,
+      initialQuestion: false,
+    });
+  }
+
+  getInitialQuestionChoices() {
+    const initialChoiceTexts = ["a little", "a lot"]
+    return <div>{initialChoiceTexts.map((text, i) => <button id={i} onClick={this.handleInitialClick}>{text}</button>)}</div>;
+  }
+
+  getSliderChoices() {
+    return [
+      "Don't know",
+      "I want to review this",
+      "Need practice",
+      "Confident",
+      "Super confident"
+    ];
+  }
+
+  getChoices() {
+    return <div>{this.getSliderChoices().map((text, i) => <button id={i} onClick={this.handleNextLevel}>{text}</button>)}</div>;
+  }
+
   render() {
+    console.log('this.state', this.state);
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h3>Step by Step placement</h3>
+        <p>{this.state.initialQuestion ? "How much previous knowledge do you have of Spanish?" : this.defaultGetCanDo(this.state.currentLevel)}</p>
+        {this.state.initialQuestion ? this.getInitialQuestionChoices() : this.getChoices()}
       </div>
     );
   }
---
+++ src/statements.js
@@ -0,0 +1,116 @@
+const statements = [
+  {
+    "level": "A1.1",
+    "question": "Q1 A1.1: How confident do you feel introducing yourself?"
+  },
+  {
+    "level": "A1.1",
+    "question": "Q2 A1.1: How confident do you feel ordering in a cafe?"
+  },
+  {
+    "level": "A1.2",
+    "question": "Q1 A1.2: How confident do you feel telling the time?"
+  },
+  {
+    "level": "A1.2",
+    "question": "Q2 A1.2: How confident do you feel reserving a hotel room?"
+  },
+  {
+    "level": "A1.2",
+    "question": "Q3 A1.2: How confident do you feel talking about your vacations?"
+  },
+  {
+    "level": "A2.1",
+    "question": "Q1 A2.1: How confident do you feel talking about your hobbies?"
+  },
+  {
+    "level": "A2.1",
+    "question":
+      "Q2 A2.1: How confident do you feel talking about your future and past holidays?"
+  },
+  {
+    "level": "A2.1",
+    "question": "Q3 A2.1: How confident do you feel talking about shopping?"
+  },
+  {
+    "level": "A2.1",
+    "question": "Q4 A2.1: How confident do you feel talking about your health?"
+  },
+  {
+    "level": "A2.2",
+    "question": "Q1 A2.2: How confident do you feel describing an appartment?"
+  },
+  {
+    "level": "A2.2",
+    "question":
+      "Q2 A2.2: How confident do you feel expressing your tastes in music?"
+  },
+  {
+    "level": "A2.2",
+    "question": "Q3 A2.2: How confident do you feel talking about your feelings?"
+  },
+  {
+    "level": "A2.2",
+    "question":
+      "Q4 A2.2: How confident do you feel talking about memories from the past?"
+  },
+  {
+    "level": "B1.1",
+    "question":
+      "Q1 B1.1: How confident do you feel expressing your opinions about work?"
+  },
+  {
+    "level": "B1.1",
+    "question":
+      "Q2 B1.1: How confident do you feel expressing your opinions about games and love?"
+  },
+  {
+    "level": "B1.1",
+    "question": "Q3 B1.1: How confident do you feel talking about housing?"
+  },
+  {
+    "level": "B1.2",
+    "question":
+      "Q1 B1.2: How confident do you feel talking about a criminal case or the media?"
+  },
+  {
+    "level": "B1.2",
+    "question": "Q2 B1.2: How confident do you feel talking about hiking?"
+  },
+  {
+    "level": "B1.2",
+    "question": "Q3 B1.2: How confident do you feel talking healthy eating?"
+  },
+  {
+    "level": "B2",
+    "question":
+      "Q1 B2: How confident do you feel talking about a barbecue evening?"
+  },
+
+  {
+    "level": "B2",
+    "question": "Q2 B2: How confident do you feel planning a ski trip?"
+  },
+
+  {
+    "level": "B2",
+    "question": "Q3 B2: How confident do you feel talking about the environment?"
+  },
+  {
+    "level": "C1",
+    "question":
+      "Q1 C1: How confident do you feel formulating an artistic opinion?"
+  },
+  {
+    "level": "C1",
+    "question":
+      "Q2 C1: How confident do you feel expressing and understanding nuanced expressions?"
+  },
+  {
+    "level": "C1",
+    "question":
+      "Q3 C1: How confident do you feel understanding subtle undertones?"
+  }
+];
+
+export default statements;
