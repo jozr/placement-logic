6bb2573f15e68158a1d3f710ac9ef6c3bc262a0c
Josephine Wright <jozright@gmail.com>
2019-01-31 17:13:50 +0100

    Refine statement data structure

---
+++ src/better-formed-statements.js
@@ -0,0 +1,45 @@
+const statements = {
+  "A1.1": {
+    0: "How confident do you feel introducing yourself?",
+    1: "How confident do you feel ordering in a cafe?"
+  },
+  "A1.2": {
+    0: "How confident do you feel telling the time?",
+    1: "How confident do you feel reserving a hotel room?",
+    2: "How confident do you feel talking about your vacations?"
+  },
+  "A2.1": {
+    0: "How confident do you feel talking about your hobbies?",
+    1: "How confident do you feel talking about your future and past holidays?",
+    2: "How confident do you feel talking about shopping?",
+    3: "How confident do you feel talking about your health?"
+  },
+  "A2.2": {
+    0: "How confident do you feel describing an apartment?",
+    1: "How confident do you feel expressing your tastes in music?",
+    2: "How confident do you feel talking about your feelings?",
+    3: "How confident do you feel talking about memories from the past?"
+  },
+  "B1.1": {
+    0: "How confident do you feel expressing your opinions about work?",
+    1: "How confident do you feel expressing your opinions about games and love?",
+    2: "How confident do you feel talking about housing?"
+  },
+  "B1.2": {
+    0: "How confident do you feel talking about a criminal case or the media?",
+    1: "How confident do you feel talking about hiking?",
+    2: "How confident do you feel talking healthy eating?"
+  },
+  "B2": {
+    0: "How confident do you feel talking about a barbecue evening?",
+    1: "How confident do you feel planning a ski trip?",
+    2: "How confident do you feel talking about the environment?"
+  },
+  "C1": {
+    0: "How confident do you feel formulating an artistic opinion?",
+    1: "How confident do you feel expressing and understanding nuanced expressions?",
+    2: "How confident do you feel understanding subtle undertones?"
+  }
+};
+
+export default statements;
